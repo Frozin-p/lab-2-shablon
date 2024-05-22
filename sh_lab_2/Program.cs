a67d42a614df079a53ca889dbef7c0c5cbb815a1
Frozin-p <nikitin278@bk.ru>
2024-05-23 00:40:07 +0300

    Добавлены классы User и Handler

--- sh_lab_2/Program.cs
+++ sh_lab_2/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace sh_lab_2
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
